be4b501350ccc64f1e652755b73f6cf296dc6144
Suprio-Das <priyamd507@gmail.com>
2025-03-21 00:53:59 +0600

    Password status shown if errors

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -18,7 +18,7 @@ const Register = () => {
         // password validation using regex
         const regex = /^(?=.*[A-Za-z])(?=.*\d).{6,}$/;
         if (!regex.test(password)) {
-            setError('Password must have at least 6 characters, including one letter and one number');
+            setError('Password must like this: SuprioDas31#');
         }
         else {
             createUserWithEmail(email, password)
@@ -41,7 +41,6 @@ const Register = () => {
                 </div>
                 <div className='w-1/2'>
                     <form onSubmit={handleRegister}>
-                        <p>{error}</p>
                         <fieldset className="fieldset">
                             <legend className="fieldset-legend">Full Name *</legend>
                             <input type="text" className="input" name='name' required placeholder="Suprio Das" />
@@ -54,6 +53,7 @@ const Register = () => {
                             <legend className="fieldset-legend">Password *</legend>
                             <input type="password" className="input" name='password' required placeholder="Type a strong password" />
                         </fieldset>
+                        <p className='text-red-600'>{error}</p>
                         <fieldset className="fieldset">
                             <legend className="fieldset-legend">Profile Photo URL *</legend>
                             <input type="text" className="input" name='photo' required placeholder="Enter photo URL" />
