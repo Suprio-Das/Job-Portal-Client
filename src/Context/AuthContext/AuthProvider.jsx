87c752a0ad0ccfb20ac565e0d076bc3678cc6c4e
Suprio-Das <priyamd507@gmail.com>
2025-03-20 23:48:25 +0600

    Create user with email and password function implemented in AuthProvider

--- src/Context/AuthContext/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react';
 import AuthContext from './AuthContext';
+import auth from '../../Firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setloading] = useState(true);
+
+    // Create user with Email and Password
+    const createUserWithEmail = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
     const authInfo = {
+        loading,
         user,
-        loading
+        createUserWithEmail
     }
     return (
         <AuthContext.Provider value={authInfo}>
