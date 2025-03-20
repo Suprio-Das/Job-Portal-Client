dc309b9787aba533d804120809a6949e5acfe20f
Suprio-Das <priyamd507@gmail.com>
2025-03-20 19:32:17 +0600

    Lottie animation loaded

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,10 +1,19 @@
+import Lottie from 'lottie-react';
 import React from 'react';
-
+import registerLottie from '../../assets/Lottie/registration.json';
 const Register = () => {
     return (
-        <div className='w-[50%] mx-auto my-5'>
+        <div className='w-[80%] mx-auto my-5'>
             <p className='text-[#3C65F5] text-center'>Register</p>
             <h1 className='text-[#05264E] text-5xl text-center font-semibold'>Start for free Today</h1>
+            <div>
+                <div>
+                    <Lottie animationData={registerLottie}></Lottie>
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
     );
 };
