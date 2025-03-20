b0d71448120e72b456323aae2cf5eb2172c008d4
Suprio-Das <priyamd507@gmail.com>
2025-03-20 23:52:44 +0600

    auth import issue solved

--- src/Context/AuthContext/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import AuthContext from './AuthContext';
-import auth from '../../Firebase';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../Firebase/Firebase.init';
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
