4479bcdaa9f4a015e8137c95e84484dcdb9e3bc0
Suprio-Das <priyamd507@gmail.com>
2025-03-20 14:53:06 +0600

    MainLayout created and outlet setup

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../Layout/MainLayout';
+import Home from '../Pages/Home/Home';
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <h1>Job Portal Root</h1>
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
     }
 ])
 
