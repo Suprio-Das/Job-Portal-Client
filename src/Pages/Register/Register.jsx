8fb1140fb8088ee27392edccbe77cf3ad0ac20fa
Suprio-Das <priyamd507@gmail.com>
2025-03-20 17:29:45 +0600

    Register Form Title added

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-            <h1>This is Register</h1>
+        <div className='w-[50%] mx-auto'>
+            <p className='text-center'>Register</p>
+            <h1 className='text-3xl text-center'>Start for free Today</h1>
         </div>
     );
 };
