0fa7c67c7a15155ca2ba3dc63428a95ff2178b01
Suprio-Das <priyamd507@gmail.com>
2025-03-20 17:58:28 +0600

    Register title modified

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div className='w-[50%] mx-auto'>
-            <p className='text-center'>Register</p>
-            <h1 className='text-3xl text-center'>Start for free Today</h1>
+        <div className='w-[50%] mx-auto my-5'>
+            <p className='text-[#3C65F5] text-center'>Register</p>
+            <h1 className='text-[#05264E] text-5xl text-center font-semibold'>Start for free Today</h1>
         </div>
     );
 };
