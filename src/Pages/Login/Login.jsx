ff4c9fc982e945e7119f18d4fd9d81190e162a19
Suprio-Das <priyamd507@gmail.com>
2025-03-21 17:42:06 +0600

    Sign-in user completed with Success/Error toast

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,18 +1,54 @@
 import Lottie from 'lottie-react';
 import LoginLottie from '../../assets/Lottie/Login.json';
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import AuthContext from '../../Context/AuthContext/AuthContext';
+import { Bounce, toast, ToastContainer } from 'react-toastify';
 
 const Login = () => {
+    const { signInUserWithEmail } = useContext(AuthContext);
     const handleSignIn = (e) => {
         e.preventDefault();
         const form = e.target;
         const email = form.email.value;
         const password = form.password.value
-        console.log(email, password)
+
+        // Sign in user
+        signInUserWithEmail(email, password)
+            .then(result => {
+                if (result.user) {
+                    toast.success('Sign in successfully', {
+                        position: "top-center",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: false,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                        transition: Bounce,
+                    });
+                }
+            })
+            .catch(error => {
+                if (error.message) {
+                    toast.error('Invalid Password or Email', {
+                        position: "top-center",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: false,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                        transition: Bounce,
+                    });
+                }
+            })
     }
     return (
         <div className='w-[80%] mx-auto my-5'>
-            {/* <ToastContainer></ToastContainer> */}
+            <ToastContainer></ToastContainer>
             <p className='text-[#3C65F5] text-center'>Welcome Back!</p>
             <h1 className='text-[#05264E] text-5xl text-center font-semibold'>Member Login</h1>
             <div className='flex justify-center items-center mt-11'>
