c8bb71715c1a9cb941239d8e6d37b5127b782f63
Suprio-Das <priyamd507@gmail.com>
2025-03-21 21:34:54 +0600

    Footer component designed

--- src/Pages/Shared/Footer.jsx
+++ src/Pages/Shared/Footer.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import JobLogo from '../../assets/job_icon.png';
 
 const Footer = () => {
     return (
-        <div>
-            This is footer.
-        </div>
+        <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
+            <aside>
+                <img src={JobLogo} alt="" />
+                <p className='text-md font-semibold'>
+                    Job-Portal
+                    <br />
+                    <span className='font-normal'>Providing reliable tech since 1992</span>
+                </p>
+            </aside>
+            <nav>
+                <h6 className="footer-title">Services</h6>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Company</h6>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Legal</h6>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </nav>
+        </footer>
     );
 };
 
