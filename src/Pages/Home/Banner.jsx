00f8e60435661e7390e161f082539cc7e268f1c7
Suprio-Das <priyamd507@gmail.com>
2025-03-22 17:23:11 +0600

    Homepage banner layout modified

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -2,14 +2,14 @@ import BannerImageOne from '../../assets/home_banner_1.png'
 const Banner = () => {
     return (
         <div className="bg-[#f2f6fd] min-h-[calc(100vh-70px)] p-11 flex items-center">
-            <div className="w-2/3 mx-auto flex items-center gap-11">
-                <div className='flex-1'>
-                    <h1 className="text-5xl font-extrabold text-[#05264e]">The <span className='text-[#3c65f5]'>Easiest Way</span> <br /> to Get Your New Job</h1>
-                    <p className='text-lg text-[#4F5E64]'>Each month, more than 3 million job seekers turn to
+            <div className="w-[90%] mx-auto flex items-center gap-10">
+                <div className='w-2/3'>
+                    <h1 className="lg:text-7xl font-extrabold text-[#05264e]">The <span className='text-[#3c65f5]'>Easiest Way</span> <br /> to Get Your New Job</h1>
+                    <p className='text-lg text-[#4F5E64] w-4/5 mt-6'>Each month, more than 3 million job seekers turn to
                         website in their search for work, making over 140,000
                         applications every single day</p>
                 </div>
-                <div className='flex-1'>
+                <div className='1/3'>
                     <img src={BannerImageOne} alt="" />
                 </div>
             </div>
