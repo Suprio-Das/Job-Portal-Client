933c498a59ffa82f2871e284b248f38b27d419c0
Suprio-Das <priyamd507@gmail.com>
2025-03-24 21:08:34 +0600

    Top Jobs component created and added to the home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import TopJobs from './TopJobs';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <TopJobs></TopJobs>
         </div>
     );
 };
---
+++ src/Pages/Home/TopJobs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopJobs = () => {
+    return (
+        <div>
+            <h1>Top jobs are here.</h1>
+        </div>
+    );
+};
+
+export default TopJobs;
