7c5693d5259d424c2f9c6e44eeb73f6b558f5567
Suprio-Das <priyamd507@gmail.com>
2025-03-24 21:59:03 +0600

    Top Jobs title added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,11 +6,17 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <div className='w-[90%] mx-auto grid grid-cols-4 gap-5'>
-                <TopJobs></TopJobs>
-                <TopJobs></TopJobs>
-                <TopJobs></TopJobs>
-                <TopJobs></TopJobs>
+            <div className='w-[90%] mx-auto my-11'>
+                <div className='mb-11'>
+                    <h1 className='lg:text-6xl font-extrabold text-center text-[#05264e]'>Jobs of the day</h1>
+                    <p className='lg:text-lg text-gray-500 text-center'>Search and connect with the right candidates faster.</p>
+                </div>
+                <div className='grid grid-cols-4 gap-5'>
+                    <TopJobs></TopJobs>
+                    <TopJobs></TopJobs>
+                    <TopJobs></TopJobs>
+                    <TopJobs></TopJobs>
+                </div>
             </div>
         </div>
     );
