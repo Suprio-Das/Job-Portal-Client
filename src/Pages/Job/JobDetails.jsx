4c5e90c549dc77bdd5caf1b1da59ae375a415708
Suprio-Das <priyamd507@gmail.com>
2025-03-25 23:16:08 +0600

    Job Details title added

--- src/Pages/Job/JobDetails.jsx
+++ src/Pages/Job/JobDetails.jsx
@@ -1,5 +1,7 @@
 import JobThumb from '../../assets/job_details_thumb.png';
 import { useLoaderData } from 'react-router-dom';
+import { BsBagCheck } from "react-icons/bs";
+import { IoLocationOutline } from "react-icons/io5"
 
 const JobDetails = () => {
     const singleJob = useLoaderData();
@@ -7,6 +9,16 @@ const JobDetails = () => {
         <div className='w-[90%] mx-auto'>
             <div>
                 <img src={JobThumb} className='rounded-2xl my-12' alt="Job_Details_Thumbnail" />
+            </div>
+            {/* Job Title and Apply Button */}
+            <div>
+                <div>
+                    <h1 className='text-[#05264e] text-3xl font-semibold'>{singleJob.title}</h1>
+                    <div className='flex gap-4'>
+                        <p className='text-xs flex items-center gap-0.5'><BsBagCheck /> {singleJob.jobType}</p>
+                        <p className='text-xs flex items-center gap-0.2'><IoLocationOutline /> {singleJob.jobType}</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
