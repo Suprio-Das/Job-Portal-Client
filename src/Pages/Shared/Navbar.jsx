484518768fbcdfd90dd6a9bac1073389a32faf6a
Suprio-Das <priyamd507@gmail.com>
2025-03-26 00:19:59 +0600

    Navbar width set to 90%

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -15,8 +15,8 @@ const Navbar = () => {
         <li><a>Item 3</a></li>
     </>
     return (
-        <div>
-            <div className="navbar bg-base-100 shadow-sm">
+        <div className='bg-base-100 shadow-sm'>
+            <div className="navbar mx-auto w-[90%]">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
