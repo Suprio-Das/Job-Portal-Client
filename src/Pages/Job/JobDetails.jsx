9b8935272f9d7fd9330ff2ae83624e0c68c867d6
Suprio-Das <priyamd507@gmail.com>
2025-03-25 23:21:21 +0600

    Apply added to the job details at the top right side of the title

--- src/Pages/Job/JobDetails.jsx
+++ src/Pages/Job/JobDetails.jsx
@@ -2,6 +2,7 @@ import JobThumb from '../../assets/job_details_thumb.png';
 import { useLoaderData } from 'react-router-dom';
 import { BsBagCheck } from "react-icons/bs";
 import { IoLocationOutline } from "react-icons/io5"
+import { GoVerified } from "react-icons/go";
 
 const JobDetails = () => {
     const singleJob = useLoaderData();
@@ -11,7 +12,7 @@ const JobDetails = () => {
                 <img src={JobThumb} className='rounded-2xl my-12' alt="Job_Details_Thumbnail" />
             </div>
             {/* Job Title and Apply Button */}
-            <div>
+            <div className='flex justify-between items-end'>
                 <div>
                     <h1 className='text-[#05264e] text-3xl font-semibold'>{singleJob.title}</h1>
                     <div className='flex gap-4'>
@@ -19,6 +20,7 @@ const JobDetails = () => {
                         <p className='text-xs flex items-center gap-0.2'><IoLocationOutline /> {singleJob.jobType}</p>
                     </div>
                 </div>
+                <button className='btn bg-[#3c65f5] text-white'><GoVerified /> Apply now</button>
             </div>
         </div>
     );
