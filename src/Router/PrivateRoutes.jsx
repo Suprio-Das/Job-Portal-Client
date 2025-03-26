3493efbd6a9efa1d884dc8364596d0475932419f
Suprio-Das <priyamd507@gmail.com>
2025-03-26 19:22:54 +0600

    Previous location fetched and send as state

--- src/Router/PrivateRoutes.jsx
+++ src/Router/PrivateRoutes.jsx
@@ -1,17 +1,20 @@
 import React, { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import AuthContext from '../Context/AuthContext/AuthContext';
 import Loader from '../Pages/Shared/Loader';
 
 const PrivateRoutes = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
+    // Getting previous location
+    const location = useLocation();
+    console.log(location);
     if (loading) {
         return <Loader></Loader>
     }
     if (user) {
         return children;
     }
-    return <Navigate to='/login'></Navigate>
+    return <Navigate to='/login' state={location}></Navigate>
 };
 
 export default PrivateRoutes;
