9cdbe92c79310412113a6f7fc8b61da21041f1de
Suprio-Das <priyamd507@gmail.com>
2025-03-27 13:00:53 +0600

    Title and Subtitle centered

--- src/Pages/JobApply/JobApply.jsx
+++ src/Pages/JobApply/JobApply.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const JobApply = () => {
     return (
         <div className='w-[90%] mx-auto shadow-sm p-5 my-5'>
-            <h1 className='text-[#05264e] text-3xl font-semibold'>Job Application</h1>
-            <p>Apply and get a chance to Boost your career.</p>
+            <div className='text-center'>
+                <h1 className='text-[#05264e] text-3xl font-semibold'>Job Application</h1>
+                <p>Apply and get a chance to Boost your career.</p>
+            </div>
         </div>
     );
 };
