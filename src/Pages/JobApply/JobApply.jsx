f17daaac4dd1c7491dc3eabac9944296707ec017
Suprio-Das <priyamd507@gmail.com>
2025-03-27 12:59:55 +0600

    Job application form title and subtitle added

--- src/Pages/JobApply/JobApply.jsx
+++ src/Pages/JobApply/JobApply.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const JobApply = () => {
     return (
-        <div>
-            This is Job Apply Page.
+        <div className='w-[90%] mx-auto shadow-sm p-5 my-5'>
+            <h1 className='text-[#05264e] text-3xl font-semibold'>Job Application</h1>
+            <p>Apply and get a chance to Boost your career.</p>
         </div>
     );
 };
