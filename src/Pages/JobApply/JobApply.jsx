e25ca235c3192032792f7f0f1fa4effda439030a
Suprio-Das <priyamd507@gmail.com>
2025-03-27 13:08:07 +0600

    Job application form layout modified

--- src/Pages/JobApply/JobApply.jsx
+++ src/Pages/JobApply/JobApply.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const JobApply = () => {
     return (
-        <div className='w-[90%] mx-auto shadow-sm p-5 my-5'>
-            <div className='text-center'>
-                <h1 className='text-[#05264e] text-3xl font-semibold'>Job Application</h1>
-                <p>Apply and get a chance to Boost your career.</p>
+        <div className='w-[90%] mx-auto  my-5'>
+            <div className='w-2/3 shadow-sm p-5 mx-auto'>
+                <div className='text-center'>
+                    <h1 className='text-[#05264e] text-3xl font-semibold'>Job Application</h1>
+                    <p>Apply and get a chance to Boost your career.</p>
+                </div>
             </div>
         </div>
     );
