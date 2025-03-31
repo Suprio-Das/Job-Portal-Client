b9ed8e5cc97bc6dfbb9a7eccff65e78e543350a0
Suprio-Das <priyamd507@gmail.com>
2025-03-31 12:27:24 +0600

    My Applications component created

---
+++ src/Pages/MyApplications/MyApplications.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyApplications = () => {
+    return (
+        <div>
+            <h2>This is my applications.</h2>
+        </div>
+    );
+};
+
+export default MyApplications;
