fc5a297645843fcf15bb843465a6b4661865e364
Suprio-Das <priyamd507@gmail.com>
2025-03-31 12:31:27 +0600

    Application title added

--- src/Pages/MyApplications/MyApplications.jsx
+++ src/Pages/MyApplications/MyApplications.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MyApplications = () => {
     return (
         <div>
-            <h2>This is my applications.</h2>
+            <h1 className='text-[#05264e] text-3xl text-center font-semibold'>My Applications</h1>
         </div>
     );
 };
