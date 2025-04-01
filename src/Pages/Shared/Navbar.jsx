fe305b64e1e3218e7f6d55f4b52101581f65c599
Suprio-Das <priyamd507@gmail.com>
2025-04-01 13:29:57 +0600

    Navbar responsive to small devices

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     </>
     return (
         <div className='bg-base-100 shadow-sm'>
-            <div className="navbar mx-auto w-[90%]">
+            <div className="navbar lg:mx-auto lg:w-[90%] w-full">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
