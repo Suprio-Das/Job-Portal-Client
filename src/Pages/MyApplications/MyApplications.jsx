111540e2a4b5be2fce3d4c8a9d5278199408c3de
Suprio-Das <priyamd507@gmail.com>
2025-04-01 10:15:16 +0600

    My applications page width modified

--- src/Pages/MyApplications/MyApplications.jsx
+++ src/Pages/MyApplications/MyApplications.jsx
@@ -1,6 +1,7 @@
 import { RiVerifiedBadgeFill } from "react-icons/ri";
 import useAuth from "../Hooks/useAuth";
 import { useEffect, useState } from "react";
+import MyApplicationsTable from "./MyApplicationsTable";
 
 const MyApplications = () => {
     const { user } = useAuth();
@@ -12,8 +13,9 @@ const MyApplications = () => {
     }, [user.email])
     console.log(appliedJobs);
     return (
-        <div>
+        <div className="w-[90%] mx-auto">
             <h1 className='text-[#05264e] text-3xl text-center font-semibold my-5 flex justify-center items-center'>My Applications <RiVerifiedBadgeFill className="text-blue-500 ml-2" /> </h1>
+            <MyApplicationsTable></MyApplicationsTable>
         </div>
     );
 };
