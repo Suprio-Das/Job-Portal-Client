e27cfb7980160429199a43d18665a3caffa7cd06
Suprio-Das <priyamd507@gmail.com>
2025-04-05 23:44:47 +0600

    Application delete implemented with alert

--- src/Pages/MyApplications/MyApplicationsTable.jsx
+++ src/Pages/MyApplications/MyApplicationsTable.jsx
@@ -1,10 +1,35 @@
 import { FaRegPenToSquare } from "react-icons/fa6";
 import { MdDoNotDisturbAlt } from "react-icons/md";
 import { Link } from 'react-router-dom';
+import Swal from "sweetalert2";
 
 const MyApplicationsTable = ({ appliedJobs }) => {
     const handleDeleteApplication = id => {
-        console.log(id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/jobApplication/${id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your application has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
     }
     return (
         <div className="overflow-x-auto">
