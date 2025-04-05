b8bf44e7455e1db6abef62088ef32491d3916351
Suprio-Das <priyamd507@gmail.com>
2025-04-05 20:01:38 +0600

    Application id fetched for update

--- src/Pages/MyApplications/UpdateMyApplication.jsx
+++ src/Pages/MyApplications/UpdateMyApplication.jsx
@@ -1,8 +1,14 @@
 import { FaDochub, FaGithub, FaLinkedin } from "react-icons/fa";
 import { BsBroadcastPin } from "react-icons/bs";
 import { FaBook } from "react-icons/fa6";
+import { useParams } from "react-router-dom";
 
 const UpdateMyApplication = () => {
+    const id = useParams();
+    const handleUpdateApplication = (e) => {
+        e.preventDefault();
+        console.log(id);
+    }
     return (
         <div className='lg:w-[90%] w-full lg:min-h-[calc(100vh-75px)] flex items-center mx-auto'>
             <div className='lg:w-2/3 w-full shadow-sm p-8 mx-auto rounded-md border-1 border-gray-400'>
@@ -10,7 +16,7 @@ const UpdateMyApplication = () => {
                     <h1 className='text-[#05264e] text-3xl font-semibold'>Update Job Application</h1>
                     <p>Update your job application before deadline crossed.</p>
                 </div>
-                <form className='flex flex-col items-center justify-center mt-5'>
+                <form className='flex flex-col items-center justify-center mt-5' onSubmit={handleUpdateApplication}>
                     <fieldset className="fieldset lg:w-1/2 w-full">
                         <legend className="fieldset-legend "><FaLinkedin className="-mr-1.5" />LinkedIn</legend>
                         <input type="url" className="input w-full" name='linkedin' placeholder="Linkedin Profile URL" />
@@ -23,7 +29,7 @@ const UpdateMyApplication = () => {
                         <legend className="fieldset-legend"><FaBook className="-mr-1.5" />Resume</legend>
                         <input type="url" className="input w-full" name='resume' placeholder="Resume URL" />
                     </fieldset>
-                    <button className="btn lg:w-1/2 w-[260px] bg-[#05264e] text-white my-3"><BsBroadcastPin /> Update</button>
+                    <button onClick={handleUpdateApplication} className="btn lg:w-1/2 w-[260px] bg-[#05264e] text-white my-3"><BsBroadcastPin /> Update</button>
                 </form>
             </div>
         </div>
