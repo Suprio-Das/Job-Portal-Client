597fed73e073afeded4120a62d02bb5260787308
Suprio-Das <priyamd507@gmail.com>
2025-04-08 22:14:47 +0600

    Add Job Post page title and padding added to the div

--- src/Pages/Job/AddJobPost.jsx
+++ src/Pages/Job/AddJobPost.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const AddJobPost = () => {
     return (
-        <div className='w-[90%] mx-auto'>
-            <h1>This is Add Job Post.</h1>
+        <div className='w-[90%] mx-auto shadow-sm px-8 py-5 my-5'>
+            <h1 className='text-3xl font-semibold'>Add New Job</h1>
         </div>
     );
 };
